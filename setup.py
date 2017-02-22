d7153d688669df0f509856cdfd6409d8e1daeaa7
Steve Coward <steve.coward@gmail.com>
2017-02-22 12:11:39 -0500

    adds README and setup.py for distribution via pypi.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='transporter',
+    version='0.1',
+    author='Steve Coward',
+    author_email='steve.coward@gmail.com',
+    url='https://github.com/stevecoward/transporter',
+    license='LICENSE.txt',
+    description='Manages the transportation of TV/Movie files to a mountable media share.',
+    long_description=open('README.txt').read(),
+    pagackes=['transporter', 'transporter.classes'],
+)
